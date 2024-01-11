efa3f3362f1d3114d40da78245adb63ffa154793
Siimki <siimki@Triins-iMac.local>
2024-01-11 15:39:44 +0200

    add 3rd chart

--- functions.js
+++ functions.js
@@ -4,9 +4,16 @@ var xpPerMonth = {};
 var modXpPerMonth = {};
 var categories = new Array
 var xpAmount = new Array
+var auditsDoneArray = new Array
+var auditsRecievedArray = new Array 
+var auditsRecievedPerMonth = new Array
+var auditsCompleted
 var projectsCompleted = {}
 var projectsCompletedPerMonth = new Array 
+var auditsDown = {};
+var auditsUp = {};
 var userId = ""
+var data
 var categories2 = [
     '2022 Nov', '2022 Dec', '2023 Jan', '2023 Feb', '2023 Mar',
     '2023 Apr', '2023 May', '2023 Jun', '2023 Jul', '2023 Aug',
@@ -19,17 +26,23 @@ var categories3 = [
     '2023-11', '2023-12', '2024-01'
 ];
 
+let query = `{
+  user {
+    id
+    login
+
+  }
+}`
+
+
 
 
 
 export async function loginUser() {
     try {
         const username = "siimkiskonen@gmail.com"; 
-        const password = "68vKFrmXqW8v7@f";
-
-     
+        const password = "68vKFrmXqW8v7@f";     
         const base64Credentials = btoa(`${username}:${password}`);
-
         const response = await fetch("https://01.kood.tech/api/auth/signin", {
             method: "POST",
             headers: {
@@ -49,9 +62,8 @@ export async function loginUser() {
 
 
 export async function display(query) {
-   // const JWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0NjQ1IiwiaWF0IjoxNzA0Mzc3Mjg2LCJpcCI6IjE4NS45Ny4yNTEuMTIxLCAxNzIuMjMuMC4yIiwiZXhwIjoxNzA0NDYzNjg2LCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsidXNlciJdLCJ4LWhhc3VyYS1jYW1wdXNlcyI6Int9IiwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoidXNlciIsIngtaGFzdXJhLXVzZXItaWQiOiI0NjQ1IiwieC1oYXN1cmEtdG9rZW4taWQiOiI1ODk5YmZkOC1iOTgwLTRkNGUtOWFhMC01MTYyMDk3ZTZmZGUifX0._gsOAwIwaV2kBWkz6w7idPkZDp_rV3j_yXTkz2GRHvk"
-    try {
-        
+
+  try {
         const response = await fetch("https://01.kood.tech/api/graphql-engine/v1/graphql",{
             method: 'POST',
             headers: {
@@ -75,15 +87,8 @@ export async function display(query) {
 }
 export async function main() {
 
-    let query = `{
-        user {
-          id
-          login
-      
-        }
-      }`
     JWT = await loginUser()
-    let data = await display(query)
+    data = await display(query)
     userId = data.data.user[0].id
     var queryXP = `query {
       transaction(
@@ -105,59 +110,83 @@ export async function main() {
     }`
     console.log(queryXP)
     let data2 = await display(queryXP)
-
-    console.log(data, "")
     let username = data.data.user[0].login
-    console.log("Username", data.data.user[0].login)
-
-    console.log(data2, "")
-    console.log("First execrcise XP: ", data2.data.transaction[0].amount, "length of", (data2.data.transaction.length))
     sumUpXP(data2)
+    greetUser(username)
+    let auditsDone = await queryAudits("up")
+    let auditsRecieved = await queryAudits("down")
+    //console.log("Audits done",auditsDone)
+     auditsCompleted, auditsDoneArray = sumUpAuditsAmount(auditsDone)
+     auditsRecievedPerMonth, auditsRecievedArray = sumUpAuditsAmount(auditsRecieved)
+   //console.log("call audits completed", auditsCompleted)
+    console.log("Audits Done Array ", auditsDoneArray)
+    console.log("call audits recieved", auditsRecievedPerMonth)
+    console.log("AuditsRecieved Peer month", auditsRecievedArray)
 
-
-    const greeting = document.getElementById("greeting")
-    if (username) {
-      greeting.innerHTML = `Welcome to the forum ${username}`
-    }
-   
-    
-    
 }
 
-
+function greetUser(username) {
+  const greeting = document.getElementById("greeting")
+  if (username) {
+    greeting.innerHTML = `Welcome to the forum ${username}`
+  }
+}
 
 export function logoutUser() {
   // Clear the JWT or session storage/local storage token
   JWT = null;
-  // If using sessionStorage or localStorage
-  // sessionStorage.removeItem('token');
-  // localStorage.removeItem('token');
-
-  // Redirect to the login page or home page
-  window.location.href = 'index.html'; // Change to your login page
+  window.location.href = 'index.html'; 
 }
 
+function sumUpAuditsAmount(data) {
+  auditsDown = {}
+  for (var month of categories3) {
+    auditsDown[month] = 0
+  }
+  var total = 0 
+  for (let i = 0; i < data.data.transaction.length; i++) {
+    total += data.data.transaction[i].amount
+    var monthYear = new Date(data.data.transaction[i].createdAt).toISOString().substring(0,7);
+    console.log("AUDITSAMOUNT",auditsDown[monthYear])
+    if (auditsDown[monthYear] != 0) {
+      auditsDown[monthYear]++
+    } 
+
+    auditsDown[monthYear] = (auditsDown[monthYear] || 1)
+
+    
+  }
+  console.log("AUDITSDOWN", auditsDown)
+  var totalPerMonth
+  var auditsPerMonth = pusthToArr(auditsDown)
+
+
+
+  
+
+  return auditsDown, auditsPerMonth
+}
 
 function sumUpXP(data) {
     var total = 0 
-    var total4 = 0
 
-    for(var month of categories3) {
+    for (var month of categories3) {
         xpPerMonth[month] = 0 
         modXpPerMonth[month] = 0
         projectsCompleted[month] = 0
+
     }
     
     for (let i = 0; i < data.data.transaction.length; i++) {
         total += data.data.transaction[i].amount
         var monthYear = new Date(data.data.transaction[i].createdAt).toISOString().substring(0,7);
         console.log("TEST",projectsCompleted[monthYear])
+
         if (projectsCompleted[monthYear] != 0) {
           projectsCompleted[monthYear]++
-          console.log("do i cum")
         }
-        projectsCompleted[monthYear] = (projectsCompleted[monthYear] || 1 ) 
 
+        projectsCompleted[monthYear] = (projectsCompleted[monthYear] || 1 ) 
         xpPerMonth[monthYear] = (xpPerMonth[monthYear] || 0) + data.data.transaction[i].amount
     }
       
@@ -208,18 +237,49 @@ export async function addChart() {
     console.log("How is chart? ")
 }
 
+export async function queryAudits(type) {
+  var queryAudit = `query {
+    transaction(
+      where: {
+        userId: { _eq: 4645 }
+        type: { _eq: ${type}}
+        path:{ _nlike: "%piscine%"}
+      },
+      order_by: { createdAt: desc }
+    ) {
+      id
+      type
+      amount
+      userId
+      attrs
+      createdAt
+      path
+    }
+  }`
+
+   data = await display(queryAudit)
+   return data
+}
+
 function renderChart() {
     var options = {
         series: [{
           name: "Desktops",
           data: xpAmount,
       }],
-        chart: {
-        width: 700,
-        height: 300,
+      chart: {
+        height: 350,
         type: 'line',
-        zoom: {
-          enabled: false
+        dropShadow: {
+          enabled: true,
+          color: '#000',
+          top: 18,
+          left: 7,
+          blur: 10,
+          opacity: 0.2
+        },
+        toolbar: {
+          show: false
         }
       },
       dataLabels: {
@@ -242,18 +302,14 @@ function renderChart() {
       xaxis: {
         categories: categories,
       },
-      responsive: [{
-        breakpoint: 1000, // applies when the screen width is <= 1000px
-        options: {
-          chart: {
-            width: '100%', // Use percentage width for responsiveness
-          },
-          legend: {
-            position: 'bottom' // Change legend position
-          },
-          // ... other options for this breakpoint
-        }
-      }]
+      legend: {
+        position: 'top',
+        horizontalAlign: 'right',
+        floating: true,
+        offsetY: -25,
+        offsetX: -5
+      }
+
       };
 
       var chart = new ApexCharts(document.querySelector("#chart"), options);
@@ -270,12 +326,19 @@ function renderProjectCompletionChart() {
         name: "Desktops",
         data: projectsCompletedPerMonth,
     }],
-      chart: {
-      width: 700,
-      height: 300,
+    chart: {
+      height: 350,
       type: 'line',
-      zoom: {
-        enabled: false
+      dropShadow: {
+        enabled: true,
+        color: '#000',
+        top: 18,
+        left: 7,
+        blur: 10,
+        opacity: 0.2
+      },
+      toolbar: {
+        show: false
       }
     },
     dataLabels: {
@@ -298,18 +361,13 @@ function renderProjectCompletionChart() {
     xaxis: {
       categories: categories,
     },
-    responsive: [{
-      breakpoint: 1000, // applies when the screen width is <= 1000px
-      options: {
-        chart: {
-          width: '100%', // Use percentage width for responsiveness
-        },
-        legend: {
-          position: 'bottom' // Change legend position
-        },
-        // ... other options for this breakpoint
-      }
-    }]
+    legend: {
+      position: 'top',
+      horizontalAlign: 'right',
+      floating: true,
+      offsetY: -25,
+      offsetX: -5
+    }
     };
 
     var chart = new ApexCharts(document.querySelector("#chart2"), options);
@@ -319,53 +377,76 @@ function renderProjectCompletionChart() {
 
 function renderAuditsChart() {
   var options = {
-      series: [{
-        name: "Desktops",
-        data: projectsCompletedPerMonth,
-    }],
-      chart: {
-      width: 700,
-      height: 300,
-      type: 'line',
-      zoom: {
-        enabled: false
-      }
+    series: [
+    {
+      name: "Audits done",
+      data: auditsDoneArray
     },
-    dataLabels: {
-      enabled: false
+    {
+      name: "Audits recieved",
+      data: auditsRecievedArray
+    }
+  ],
+    chart: {
+    height: 350,
+    type: 'line',
+    dropShadow: {
+      enabled: true,
+      color: '#000',
+      top: 18,
+      left: 7,
+      blur: 10,
+      opacity: 0.2
     },
-    stroke: {
-      curve: 'straight',
-      width: 5
+    toolbar: {
+      show: false
+    }
+  },
+  colors: ['#77B6EA', '#545454'],
+  dataLabels: {
+    enabled: true,
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  title: {
+    text: 'Audits done and recieved!',
+    align: 'left'
+  },
+  grid: {
+    borderColor: '#e7e7e7',
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
     },
+  },
+  markers: {
+    size: 1
+  },
+  xaxis: {
+    categories: categories,
     title: {
-      text: 'Audits done',
-      align: 'left'
+      text: 'Month'
+    }
+  },
+  yaxis: {
+    title: {
+      text: 'Audits completed per month'
     },
-    grid: {
-      row: {
-        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-        opacity: 0.5
-      },
-    },
-    xaxis: {
-      categories: categories,
-    },
-    responsive: [{
-      breakpoint: 1000, // applies when the screen width is <= 1000px
-      options: {
-        chart: {
-          width: '100%', // Use percentage width for responsiveness
-        },
-        legend: {
-          position: 'bottom' // Change legend position
-        },
-        // ... other options for this breakpoint
-      }
-    }]
-    };
+    min: 5,
+    max: 40
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'right',
+    floating: true,
+    offsetY: -25,
+    offsetX: -5
+  }
+  };
 
-    var chart = new ApexCharts(document.querySelector("#chart3"), options);
-    chart.render();
+  var chart = new ApexCharts(document.querySelector("#chart3"), options);
+  chart.render();
   
 }
+
